bf8398e2ac2523ff7afff861ad2c24dc78b73451
Christoph Hafner <tibius74@gmail.com>
2024-11-25 21:01:47 +0000

    Package "Hafner.Compatibility.DynamicallyAccessedMembersAttribute" added.

---
+++ Source/Hafner.Compatibility.DynamicallyAccessedMembersAttribute/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+[assembly: Guid("79DA9D5A-D62B-4239-8BC1-3AD826A66406")]
--- Source/Hafner.Compatibility.NullableReferenceTypeAttributes.G1/BuildEvents/PostPackage.bat
+++ Source/Hafner.Compatibility.NullableReferenceTypeAttributes.G1/BuildEvents/PostPackage.bat
@@ -13,7 +13,7 @@ MD..\..\Artefacts >nul 2>nul
 REM Publish the NuGet package to nuget.org and the Artefacts folder
 FOR /R %%a IN (*.nupkg) DO (
   XCOPY "%%a" "..\..\Artefacts" /R
-  NUGET push %%a -Source https://api.nuget.org/v3/index.json
+  NUGET push %%a -Source https://api.nuget.org/v3/index.json -SkipDuplicate
 )
 
 :EOF
