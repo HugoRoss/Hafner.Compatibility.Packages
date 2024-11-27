ddb666ef4bcb263bedb4720145f1e9c56442c5e1
Christoph Hafner <tibius74@gmail.com>
2024-11-27 22:08:58 +0000

    Project/package "Hafner.Compatibility.LoaderOptimizationAttribute" added.

---
+++ Source/Hafner.Compatibility.LoaderOptimizationAttribute/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+[assembly: ComVisible(false)]
+[assembly: CLSCompliant(true)]
+[assembly: Guid("39441EFD-8A81-4EE0-B689-237BDD328BC9")]
---
+++ Source/Hafner.Compatibility.LoaderOptimizationAttribute/LoaderOptimization.cs
@@ -0,0 +1,20 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace System;
+
+using System.Diagnostics.CodeAnalysis;
+
+[SuppressMessage("Design", "CA1069:Enums values should not be duplicated", Justification = "That's how it is defined.")]
+public enum LoaderOptimization {
+
+    [Obsolete("LoaderOptimization.DisallowBindings has been deprecated and is not supported.")]
+    DisallowBindings = 4,
+    [Obsolete("LoaderOptimization.DomainMask has been deprecated and is not supported.")]
+    DomainMask = 3,
+    MultiDomain = 2,
+    MultiDomainHost = 3,
+    NotSpecified = 0,
+    SingleDomain = 1,
+
+}
